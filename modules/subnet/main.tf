933e77043fa7706a360c8840c7f36e346ace67ba
el-pendeloco <el-pendeloco@users.noreply.github.com>
2026-07-31 10:32:23 -0400

    bugfix: fixed a bug in the linting ci script
    - Reformatted proper

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 resource "aws_subnet" "subnet" {
-  vpc_id = var.vpc_id
+  vpc_id            = var.vpc_id
   cidr_block        = var.cidr_block
   availability_zone = var.availability_zone
 
